ae6971ce039652316ae30af8b4ab633d906d84df
Vealke ⠀ <koristaosu1337@gmail.com>
2025-12-25 10:14:23 +0400

    BAKES BAKES BAKES

--- src/scrapper/scrap.py
+++ src/scrapper/scrap.py
@@ -1,24 +1,45 @@
 import requests
+import time
 import re
 
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright, expect
 
-response = requests.get("https://jollyposhfoods.com/collections/savoury-pies?srsltid=AfmBOopTgRpEmH-59rHJxhT1ya20TA9WFiRg_nWJuIEfcE30MkRtoJcg")
-soup = BeautifulSoup(response.text, 'lxml')
-names = soup.find_all(class_="grid-product__title")
+def sync():
+    with sync_playwright() as s:
+        browser = s.chromium.launch()
+        page = browser.new_page()
+        page.goto('https://www.greggs.com/menu?category=savouries-bakes')
+        page.screenshot(path='./img/bakes.png', full_page=True)
 
-stopwords = ["14oz", "10oz", "8oz",]
+        bakes = page.get_by_text("Bake").all_inner_texts()
 
-all_names = ""
+        for _ in range(3):
+            bakes.pop(0)
 
-for name in names:
-    all_names += name.text + ","
+        for i in range(len(bakes)):
+            print(bakes[i])
 
-str = all_names.split(',')
+        browser.close()
 
-for word in str:
-    sorted = filter(lambda w: w not in stopwords, re.split(r'\W+', word.lower()))
-    print(list(sorted))
+sync()
 
-# I've stolen this
-# reduced = filter(lambda w: w not in stopwords, re.split(r'\W+', sentence.lower()))
+# If holly playwright won't help:
+
+# from bs4 import BeautifulSoup
+
+# response = requests.get("https://jollyposhfoods.com/collections/savoury-pies?srsltid=AfmBOopTgRpEmH-59rHJxhT1ya20TA9WFiRg_nWJuIEfcE30MkRtoJcg")
+# soup = BeautifulSoup(response.text, 'lxml')
+# names = soup.find_all(class_="grid-product__title")
+
+# stopwords = ["14oz", "10oz", "8oz"]
+
+# all_names = ""
+
+# for name in names:
+#     all_names += name.text + "\n"
+
+# str = all_names.split('\n')
+
+# for word in str:
+#     sorted = filter(lambda w: w not in stopwords, re.split(r'\W+', word.lower()))
+#     print(list(sorted))
