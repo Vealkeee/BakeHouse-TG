fa3161278ddb79c4d01e5aa06e982cc273ccf922
Vealke ⠀ <koristaosu1337@gmail.com>
2025-12-25 18:24:02 +0400

    many keyboards

--- src/Handlers/keyboards.py
+++ src/Handlers/keyboards.py
@@ -8,10 +8,63 @@ from aiogram.types import (
 start_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text="🥧 MENU", callback_data="menu")
+            InlineKeyboardButton(text="🥧 MENU", callback_data="menu"),
+            InlineKeyboardButton(text="⚙️ INFO", callback_data="info")
         ],
         [
-            InlineKeyboardButton(text="⚙️ INFO", callback_data="info")
+            InlineKeyboardButton(text="👤 My Account", callback_data="account")
+        ]
+    ]
+)
+
+info_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="📖 About", callback_data="about")
+        ],
+        [
+            InlineKeyboardButton(text="🗺️ Language", callback_data="lan")
+        ]
+    ]
+)
+
+info_back = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="❌ Previous", callback_data="back")
+        ]
+    ]
+)
+
+categories = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="🥐 Savouries & Bakes", callback_data="food_menu")
+        ],
+        [
+            InlineKeyboardButton(text="☕ Drinks & Snacks", callback_data="drinks_menu")
+        ]
+    ]
+)
+
+Food_category = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="🥐 Bakes", callback_data="bakes")
+        ],
+        [
+            InlineKeyboardButton(text="🥖 Pizza", callback_data="pizza")
+        ]
+    ]
+)
+
+Drinks_category = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="☕ Drinks", callback_data="drink")
+        ],
+        [
+            InlineKeyboardButton(text="🍫 Snacks", callback_data="Snacks")
         ]
     ]
 )
